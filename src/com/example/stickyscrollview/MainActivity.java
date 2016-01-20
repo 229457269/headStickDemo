6e1bb21bf476a40c11e7a06ad26f8d95981fe28f
zhaoqiang <229457269@qq.com>
2016-01-20 17:47:13 +0800

    头部悬停效果的实现

---
+++ src/com/example/stickyscrollview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.stickyscrollview;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+/**
+ * 悬停布局
+ * @author Administrator
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+}
